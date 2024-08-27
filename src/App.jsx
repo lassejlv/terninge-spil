90de368013f5f474d4cc221a0d8e89a70a4ad353
lassejlv <vestergaardlasse2@gmail.com>
2024-08-27 12:44:14 +0200

    Add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Button } from './components/Button';
+
 export default function App() {
   return (
     <div>
       <h1>Hello World</h1>
+
+      <Button>Click Me</Button>
     </div>
   );
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  background-color: aqua;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: 600;
+  padding: 10px 20px;
+
+  &:hover {
+    background-color: blue;
+  }
+`;
