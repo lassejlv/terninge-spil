4953994dfa5b7af32944c01726baa6eea8549218
Razzoy <1081892@ucn.dk>
2024-08-28 08:42:45 +0200

    stable

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { Button } from './components/Button';
+import { Dice } from './components/Dice/Dice';
+import { Player } from './components/Player/Player';
+import { useEffect, useState } from 'react';
 
 export default function App() {
   return (
