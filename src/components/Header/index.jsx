3f4335afb54d0217a30b920d0b8964e3a13bef8f
Razzoy <1081892@ucn.dk>
2024-08-29 21:30:36 +0200

    Styling fix

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,18 @@ import styled from 'styled-components';
 
 export const Header = styled.h1`
   color: #290b41;
+  font-size: 3rem;
+  text-align: center;
+  margin: 0;
+`;
+export const Header2 = styled.h2`
   font-size: 2rem;
   text-align: center;
-  margin-bottom: 2rem;
+  background-color: #00000083;
+  color: #e89c3f;
+  width: fit-content;
+  margin: 0.5rem auto;
+  border: 3px solid #de8c21;
+  border-radius: 10px;
+  padding: 1rem;
 `;
