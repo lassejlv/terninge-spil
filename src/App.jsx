ad9ee083e1741fe7369d90ab217fdcbc6be91986
Razzoy <1081892@ucn.dk>
2024-08-30 10:03:04 +0200

    button delay - change header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Container } from './components/Container';
 import { Header } from './components/Header';
-import { Header2 } from './components/Header';
 import { PlayerBoard } from './components/PlayerBoard/PlayerBoard';
 import { useState } from 'react';
 import style from './components/PlayerBoard/PlayerBoard.module.scss';
@@ -12,7 +11,6 @@ export default function App() {
   return (
     <Container>
       <Header>21 Dice Game</Header>
-      <Header2>It is Player x's turn</Header2>
       <div className={style.containerPlayerBoard}>
         <PlayerBoard player='Player 1' avatar='../src/assets/img/player1.jpg' points={points}></PlayerBoard>
         <PlayerBoard player='Player 2' avatar='../src/assets/img/player2.jpg' points={points}></PlayerBoard>
