c386235c4fcd512ca5fd80351eb1efd6161f4938
Evelina-Ok <142876802+Evelina-Ok@users.noreply.github.com>
2024-08-30 11:26:11 +0200

    #6

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -9,14 +9,3 @@ export const Header = styled.h1`
   text-align: center;
   margin: 0;
 `;
-export const Header2 = styled.h2`
-  font-size: 1.5rem;
-  text-align: center;
-  background-color: #00000083;
-  color: #e89c3f;
-  width: fit-content;
-  margin: 0.5rem auto 1rem;
-  border: 3px solid #de8c21;
-  border-radius: 10px;
-  padding: 1rem;
-`;
--- src/components/PlayerBoard/Player/Player.jsx
+++ src/components/PlayerBoard/Player/Player.jsx
@@ -1,7 +1,10 @@
 import style from './Player.module.scss'
-export function Player({ diceSum, points, player, avatar }) {
+export function Player({ diceSum, points, player, avatar, turn }) {
     return (
         <>
+        <div className={style.turnContainer}>
+                <h2>{turn}</h2>
+            </div>
             <h3>{player}</h3>
             <img className={style.playerAvatar} src={avatar} />            
             <h2>{diceSum}</h2>
